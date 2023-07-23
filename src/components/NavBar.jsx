8e4a674b974752000f6e8f883d80eae22392f3f7
Kristinn <kris@Kristinns-MacBook-Air.local>
2023-07-23 13:07:31 -0400

    add nav links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import Logo from "../assets/logo.png";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]"></div>
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={Logo} alt="Logo Image" style={{ width: "50px" }} />
+      </div>
+      <div>
+        <ul className="flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      <div className="hidden">
+        <FaBars />
+      </div>
+
+      <ul className="hidden">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+    </div>
   );
 };
+
+export default NavBar;
