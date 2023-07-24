93b360357db7d09629b46184ebe5cf7be603bc04
Kristinn <kris@Kristinns-Air.attlocal.net>
2023-07-24 10:42:08 -0400

    add hamburger menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,33 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/logo.png";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
         <img src={Logo} alt="Logo Image" style={{ width: "50px" }} />
       </div>
-      <div>
-        <ul className="flex">
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-
-      <div className="hidden">
-        <FaBars />
-      </div>
-
-      <ul className="hidden">
+      <ul className="hidden md:flex">
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
         <li>Work</li>
         <li>Contact</li>
+      </ul>
+
+      <div className="md:hidden">
+        <FaBars />
+      </div>
+
+      <ul className="hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center">
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
       </ul>
     </div>
   );
