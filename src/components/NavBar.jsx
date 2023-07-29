faf2784185d0cfa22a7bfc539ca87a98a32ca9fa
Kristinn <kris@Kristinns-MacBook-Air.local>
2023-07-29 15:26:30 -0400

    edit mobile menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,11 +4,15 @@ import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
         <img src={Logo} alt="Logo Image" style={{ width: "50px" }} />
       </div>
+
+      {/* menu */}
       <ul className="hidden md:flex">
         <li>Home</li>
         <li>About</li>
@@ -17,17 +21,28 @@ const NavBar = () => {
         <li>Contact</li>
       </ul>
 
-      <div className="md:hidden">
+      {/* Hamburger */}
+      <div onClick={handleClick} className="md:hidden">
         <FaBars />
       </div>
 
-      <ul className="hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center">
+      {/* mobile menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
         <li className="py-6 text-4xl">Home</li>
         <li className="py-6 text-4xl">About</li>
         <li className="py-6 text-4xl">Skills</li>
         <li className="py-6 text-4xl">Work</li>
         <li className="py-6 text-4xl">Contact</li>
       </ul>
+
+      {/* social iconsm */}
+      <div className="hidden"></div>
     </div>
   );
 };
