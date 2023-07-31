f2bbe51c0ee9a3fdcfb44ebbf811fa5d4c4b304a
Kristinn <kris@Kristinns-Air.hsd1.fl.comcast.net>
2023-07-31 14:28:32 -0400

    add additional icons

---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+import Raect from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return <div className="w-full h-screen bg-[#a192f]"></div>;
+};
+
+export default Home;
