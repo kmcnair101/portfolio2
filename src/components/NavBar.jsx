210029f88098846bab3b419adb0d4481e6aa067b
Kristinn <kris@kristinns-air.attlocal.net>
2023-08-01 16:00:09 -0400

    add home text

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <img src={Logo} alt="Logo Image" style={{ width: "50px" }} />
+        <img src={Logo} alt="Logo Image" style={{ width: "200px" }} />
       </div>
 
       {/* menu */}
@@ -28,7 +28,7 @@ const NavBar = () => {
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
-      {/* mobile menu */}
+      {/* Mobile menu */}
       <ul
         className={
           !nav
@@ -43,7 +43,7 @@ const NavBar = () => {
         <li className="py-6 text-4xl">Contact</li>
       </ul>
 
-      {/* social icons */}
+      {/* Social icons */}
       <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
