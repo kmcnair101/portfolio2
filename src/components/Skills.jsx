b65e858d9126fba18664c9176d44aa7e43e1f763
Kristinn <kris@Kristinns-MacBook-Air.local>
2023-08-02 10:28:38 -0400

    import files for section

---
+++ src/components/Skills.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import HTML from "../assets/html.png";
+import CSS from "../assets/html.png";
+import JavaScript from "../assets/html.png";
+import ReactImg from "../assets/html.png";
+import Node from "../assets/html.png";
+import FireBase from "../assets/html.png";
+import AWS from "../assets/html.png";
+import Github from "../assets/github.png";
+import Tailwind from "../assets/tailwind.png";
+import Mongo from "../assets/mongo.png";
+
+const Skills = () => {
+  return <div name="skills"></div>;
+};
+
+export default Skills;
