58ea2e6a2cad8ed12b04a27ecbf23e8305149b2e
Kristinn <kris@Kristinns-MacBook-Air.local>
2023-08-09 15:59:58 -0400

    change text in hero

--- src/components/UI/Services.jsx
+++ src/components/UI/Services.jsx
@@ -27,7 +27,7 @@ const Services = () => {
           <div className="w-full py-3 px-2 sm:max-w-xl sm:mx-auto sm:px-0">
             <div className="relative text-gray-700 antialiased text-sm font-semibold">
               {/* =========== vertical line running through the middle ============ */}
-              <div className="hidden absolute w-1 sm:block bg-indigo-300 h-full left-1/2 transform -translate-x-1/2"></div>
+              <div className="hidden absolute w-1 sm:block bg-teal-400 h-full left-1/2 transform -translate-x-1/2"></div>
 
               {/* =========== left card ================= */}
               <div className="mt-6 sm:mt-0 sm:mb-12">
