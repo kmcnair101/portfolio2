96db02da7e461147689f92fdfcb04efeba3d9e9a
Kristinn <kris@Kristinns-MacBook-Air.local>
2023-08-09 14:00:35 -0400

    change format

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import NavBar from "./components/NavBar";
-import Home from "./components/Home";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
+import { useEffect } from "react";
+import Aos from "aos";
+
+import "./App.css";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/UI/Hero";
+import Services from "./components/UI/Services";
+import Portfolio from "./components/UI/Portfolio";
+import Contact from "./components/UI/Contact";
 
 function App() {
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
   return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-      <Skills />
-      <Projects />
-    </div>
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <Services />
+        <Portfolio />
+        <Contact />
+      </main>
+      <Footer />
+    </>
   );
 }
 
