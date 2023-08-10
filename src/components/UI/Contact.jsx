db8e13fab4be2185e342f34121f17bfc607dd76c
Kristinn <kris@Kristinns-MacBook-Air.local>
2023-08-10 13:17:43 -0400

    adjusted map

--- src/components/UI/Contact.jsx
+++ src/components/UI/Contact.jsx
@@ -4,14 +4,12 @@ const Contact = () => {
   return (
     <section id="contact" className="pb-16">
       <div className="container">
-        <h2 className="text-headingColor font-[700] text-[2.5rem] mb-8">
-          Get in touch
-        </h2>
+        <h2 className=" font-[700] text-[2.5rem] mb-8">Get in touch</h2>
         <div className="md:flex justify-between items-center">
           <div className="w-full md:w-1/2 h-[300px] sm:h-[450px]">
             <iframe
               title="google-maps"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57903.13026081353!2d91.81978234479769!3d24.899835746993343!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x375054d3d270329f%3A0xf58ef93431f67382!2sSylhet!5e0!3m2!1sen!2sbd!4v1679444673359!5m2!1sen!2sbd"
+              src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=miami+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
               className="border-0 w-full h-full"
               allowFullScreen=""
               loading="lazy"
@@ -19,7 +17,7 @@ const Contact = () => {
             ></iframe>
           </div>
 
-          <div className="w-full mt-8 md:mt-0 md:w-1/2 sm:h-[450px] lg:flex items-center bg-indigo-100 px-4 lg:px-8 py-8">
+          <div className="w-full mt-8 md:mt-0 md:w-1/2 sm:h-[450px] lg:flex items-center bg-black px-4 lg:px-8 py-8">
             <form className="w-full">
               <div className="mb-5">
                 <input
@@ -52,7 +50,7 @@ const Contact = () => {
                 />
               </div>
 
-              <button className="w-full p-3 focus:outline-none rounded-[5px] bg-smallTextColor text-white hover:bg-headingColor text-center ease-linear duration-150">
+              <button className="w-full p-3 focus:outline-none rounded-[5px] bg-smallTextColor hover:bg-headingColor text-center ease-linear duration-150">
                 Send Message
               </button>
             </form>
