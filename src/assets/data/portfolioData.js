dae8813697df9e6ccaddc7837272e70222392aea
Kristinn <kmcnair_100@outlook.com>
2023-09-18 13:46:05 -0400

    add restaurant demo source code

--- src/assets/data/portfolioData.js
+++ src/assets/data/portfolioData.js
@@ -1,17 +1,28 @@
 import portfolioImg01 from "../images/portfolio-01.png";
 import portfolioImg02 from "../images/portfolio-02.png";
-import portfolioImg03 from "../images/portfolio-03.png"
+import portfolioImg03 from "../images/portfolio-03.png";
+import PortfolioImg04 from "../images/portfolio-04.png";
 
 
 const portfolios = [
+  {
+    id: "04",
+    imgUrl: portfolioImg03,
+    category: "Web",
+    title: "Munch App",
+    description: "A restaurant reservation and rewards application for miami based restaurant.",
+    technologies: ["HTML", "SCSS", "JavaScript", "NextJS", "Axios", "Firebase", "Google Maps API", "Google Places API"],
+    siteUrl: "",
+    codeUrl: "https://github.com/kmcnair101/munched",
+  },
   {
     id: "03",
     imgUrl: portfolioImg03,
     category: "Web",
     title: "Portfolio",
     description:
-      "The code to my portfolio website",
-    technologies: ["React", "Tailwind css"],
+      "The code to the portfolio website",
+    technologies: ["HTML", "JavaScript", "React", "Tailwind css"],
     siteUrl: "https://mcnairkris.com/",
     codeUrl: "https://github.com/kmcnair101/portfolio2",
   },
